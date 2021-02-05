f030a8daf569d360a21f19a5960d69de85c4e69f
Wendi Freeman <wfreeman2112@gmail.com>
2021-02-05 13:21:10 -0600

    fix: refactor hard coded connection string to Heroku config var

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const uri = process.env.MONGODB_URI;
 
 //create port
 const PORT = process.env.PORT || 3000;
