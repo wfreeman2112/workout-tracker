ad08619dfcc490988b82bf3d607226119ea300e9
Wendi Freeman <wfreeman2112@gmail.com>
2021-02-05 09:55:16 -0600

    starting routes in api.js

--- models/workoutModel.js
+++ models/workoutModel.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const Workout = mongoose.model("Workout", WorkoutSchema);
+const Workout = mongoose.model("Workout", workoutSeed);
 
 module.exports = Workout;
