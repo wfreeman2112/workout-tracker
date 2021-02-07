a66e30cfb8d278fd8456cf4ac71e127a2201a34b
Wendi Freeman <wfreeman2112@gmail.com>
2021-02-07 10:59:55 -0600

    created index.js

---
+++ routes/api.js
@@ -0,0 +1,14 @@
+const router = require("express").Router();
+const Workout = require("../models/workoutModel.js");
+
+
+//establish routes- 
+// Add exercises to the most recent workout plan.
+
+// Add new exercises to a new workout plan.
+
+// View the combined weight of multiple exercises from the past seven workouts on the stats page.
+
+// View the total duration of each workout from the past seven workouts on the stats page.
+
+module.exports = router;
