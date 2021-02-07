fb3474e8faa44c0fba7a583e7f7d0a5351947440
Wendi Freeman <wfreeman2112@gmail.com>
2021-02-07 11:59:59 -0600

    api routes created

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,10 +1,54 @@
 const router = require("express").Router();
-const Workout = require("../models/workoutModel.js");
+const db = require("../models");
 
-
-//establish routes- 
+//establish routes-
 // Add exercises to the most recent workout plan.
+router.get("/api/workouts", function (req, res) {
+  db.Workout.find().then(function (workoutdb) {
+    res.json(workoutdb);
+  });
+});
 
+router.post("/api/workouts", function (req, res) {
+  db.Workout.create(req.body).then(function (workoutdb) {
+    res.json(workoutdb);
+  });
+});
+
+router.put("/api/workouts/:id", function (req, res) {
+  db.Workout.updateOne(
+    {
+      _id: req.params.id,
+    },
+    {
+      $push: {
+        exercises: req.body,
+      },
+    }
+  ).then(function (workoutdb) {
+    res.json(workoutdb);
+  });
+});
+
+router.get("/api/workouts/range", function (req, res) {
+    db.Workout.find()
+    .then(function (workoutdb){
+        res.json(workoutdb);
+    })
+    
+    module.export = router 
+    
+})
+
+// router.get("/api/transaction", (req, res) => {
+//     Transaction.find({})
+//       .sort({ date: -1 })
+//       .then(dbTransaction => {
+//         res.json(dbTransaction);
+//       })
+//       .catch(err => {
+//         res.status(400).json(err);
+//       });
 // Add new exercises to a new workout plan.
 
 // View the combined weight of multiple exercises from the past seven workouts on the stats page.
